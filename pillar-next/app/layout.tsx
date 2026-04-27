1fe19393a1deee5effdf06abd31e967f8f039e99
Shreyansh <pillar.coach@gmail.com>
2026-04-27 14:07:54 -0500

    added vercel website tracking

--- pillar-next/app/layout.tsx
+++ pillar-next/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter_Tight } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import ThemeProvider from '@/components/ThemeProvider'
 import './globals.css'
 
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
